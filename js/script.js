96c6dd483300cf3c2feb2bcb45cc3a7276e144b4
Maxim <ttoptpet@mail.ru>
2023-10-13 20:16:08 +0300

    sidebar components done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 function onDropdown(id) {
-  
   document.getElementById(`dropdownMenu${id}`).classList.toggle("dropdown_active");
-  document.getElementById(`dropdownButton${id}`).classList.toggle("table__item__link__icon_more_active");
-        
+  document.getElementById(`dropdownButton${id}`).classList.toggle("table__item__link__icon_more_active");    
+}
+
+function onSwitch() {
+
+  document.getElementById(`switch`).classList.toggle("create-post__item_switch__btn_on");
+  const text = document.getElementsByClassName('create-post__item_switch__text');
+  for (let i = 0; i < text.length; i++) {
+    text[i].classList.toggle('create-post__item_switch__text_active')
+  }
+  
 }
